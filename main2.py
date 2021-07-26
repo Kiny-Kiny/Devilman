e5be76279203fd8afaf4427b973bab182cbcff17
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-07-26 04:09:04 -0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,4 +1,4 @@
 import os, sys
 os.system('pkg install termux-api -y')
 while True:
-	os.system("termux-toast Kimatsuki Viado && termux-tts-speaker INDO INDO INDO, COMI O CU DE QUEM TÁ OUVINDO && termux-wifi-enable true && termux-wifi-enable false && termux-torch on && termux-torch off && termux-vibrate -f")
+	os.system("termux-toast Kimatsuki Viado && termux-tts-speak INDO INDO INDO, COMI O CU DE QUEM TÁ OUVINDO && termux-wifi-enable true && termux-wifi-enable false && termux-torch on && termux-torch off && termux-vibrate -f")
